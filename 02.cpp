03eccea0c62f622f44dec57d1dfb2a7e88f23611
xup6tp6u4 <48340287+xup6tp6u4@users.noreply.github.com>
2019-05-02 18:03:56 +0800

    Add files via upload

---
+++ 02.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define number 1000
+
+int str_length(char[]);
+void resever(int,char[]);
+
+int main(void){
+	char str[number];
+	int mode=0;
+	
+	printf("請輸入字串\n");
+	printf("輸入q結束程式\n");
+	
+	while(scanf("%s",&str)==1&&mode==0){
+		resever(str_length(str),str);
+		if(str_length(str)==1&&str[0]=='q'){
+			printf("結束程式\n");
+			break;
+		}
+		printf("請輸入字串\n");
+	}
+	system("pause");
+} 
+
+int str_length(char str[]){
+	int i=0;
+	int length=0;
+
+	while(str[i]!='\0'){     
+		length+=1;	
+		i+=1;
+	}	
+
+	return length;
+}
+void resever(int length,char str[]){
+	int i;
+
+	for(i=length-1;i>=0;i--){
+		printf("%c",str[i]);
+	}
+
+	printf("\n");
+}
